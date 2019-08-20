9babd1f310e32e08012c144f53308c90e8afcd1c
ilhamgusti <ilhamgewwe@gmail.com>
2019-08-20 13:53:54 +0700

    Change into styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './components/NavBar/NavBar'
+import IconBox from './components/InfoBox/IconBox'
+import InfoBox from './components/InfoBox/InfoBox'
+import Section from './Pages/Home/Section'
+import Section2 from './Pages/Home/Section2'
+import JobsBox from './components/InfoBox/JobsBox'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<React.Fragment>
+			<NavBar />
+			<Section />
+			<Section2 />
+			<InfoBox>
+				{/* <img
+					src={logo}
+					onMouseEnter={scaleUp}
+					onMouseLeave={scaleDown}
+					ref={element => {
+						logoElement = element
+					}}
+					className="App-logo"
+					alt="logo"
+				/> */}
+				<h1>ILHAM GUSTI WIBOWO</h1>
+				<JobsBox textAlign="right" first="#fada21" second="#fa981d">
+					<h4>Web Developer & Software Engineer</h4>
+				</JobsBox>
+				<IconBox />
+			</InfoBox>
+		</React.Fragment>
+	)
 }
 
-export default App;
+export default App
